5c901667dcfdd7f689884a14c67a1fbf2fdd8134
RuaYiii <ylzwns@sina.com>
2020-02-23 23:42:14 +0800

    not first 2020/2/23

---
+++ Falsk_test.py
@@ -0,0 +1,34 @@
+'''$env:FLASK_APP = "Flask_test.py"'''
+# set FLASK_ENV=development
+from flask import Flask
+from flask import escape
+from flask import url_for
+from werzeug.debug import DebugApplication
+from myapp import app
+app= DebugApplication(app,evalex= True)
+app= Flask(__name__)
+@app.route('/')
+def index():
+    return "hello WORLD!!!!"
+@app.route('/login')
+def login():
+    return 'login!!!!!!!'
+@app.route('/hello')
+def hello():
+    return "test holeeeasx;askx;ak;XKA;JXKAJX"
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return '{} \' s prrfile !!  '.format(escape(username)) #escape??'''
+@app.route("/post/<int:post_id>")
+def show_port(post_id):
+    return 'post %d' % post_id
+@app.route("/path/<path:subpath>")
+def show_subpath(path):
+    return 'subpathe %s ' %escape(path)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login',next= '/'))
+    print(url_for('show_user_profile',username= "Jam"))
+
+''' '''
